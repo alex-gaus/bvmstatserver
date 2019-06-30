4a684f23b34526c8da2635df870db48578ae4475
gobitodic <alexander@gaus-fellbach.de>
2019-06-30 11:16:46 +0200

    klammer

--- kill.py
+++ kill.py
@@ -8,12 +8,12 @@ def killer():
     df = pd.read_sql_query("SELECT Id, Command, Time from information_schema.processlist", conn)
     x = 0
     while x < len(df):
-        kill_id = df["Id"][x]
+        kill_id = str(df["Id"][x])
         command = df["Command"][x]
         time = str(df["Time"][x])
         if time > 50 or command == "Sleep":
             try:
-                pd.read_sql_query("Kill %s"%(kill_id)
+                pd.read_sql_query("Kill %s"%(kill_id))
                 logging.info("Process %s killed!"%(kill_id))
             except:
                 logging.info("Couldn't kill process %s"%(kill_id))
