920da2239f1e3028dd390502bc45f183968635dd
gobitodic <alexander@gaus-fellbach.de>
2019-06-30 11:47:44 +0200

    fhjdkgghj

--- kill.py
+++ kill.py
@@ -22,22 +22,23 @@ def killer():
         command = df["Command"][x]
         time = int(df["Time"][x])
         if time > 20 and command == "Sleep":
-            try:
                 try:
                     connection = mysql.connector.connect(host="gobitodic.mysql.pythonanywhere-services.com", user="gobitodic", passwd="subotica", db="gobitodic$reports")
                 except:
                     logging.error("Keine Verbindung zum Server")
                     exit(0)
                 cursor = connection.cursor()
-                cursor.execute("Kill %s"%(kill_id))
-                logging.info("Process %s killed!"%(kill_id))
-                cursor.close()
-            except:
-                logging.info("Couldn't kill process %s"%(kill_id))
+                try:
+                    cursor.execute("Kill %s"%(kill_id))
+                    logging.info("Process %s killed!"%(kill_id))
+                    cursor.close()
+                    connection.close()
+                except:
+                    logging.info("Couldn't kill process %s"%(kill_id))
+                    try:
+                        cursor.close()
+                        connection.close()
+                        logging.info("Couldn't close connection")
         x = x+1
-        try:   
-            connection.close()
-        except:
-            logging.info("connection could not be closed")
 
 
